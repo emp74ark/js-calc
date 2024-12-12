5ff37314bc8460fa331224481d5714e8ba10ab15
Andrei Yurkouski <emp74ark@gmail.com>
2024-12-12 23:04:29 +0100

    feat: percentage calculation

--- src/js/actions.js
+++ src/js/actions.js
@@ -1,9 +1,17 @@
 export function calculate(values) {
   try {
+    if (/\d%/.test(values)) {
+      values = values.replaceAll(/\d%/g, (match) => {
+        return (parseInt(match.replace('%', '')) / 100);
+      });
+    }
+
     const result = new Function(`return ${ values }`)();
+
     if (isNaN(result) || !isFinite(result)) {
       return 0;
     }
+
     return result;
   } catch (error) {
     return 0;
@@ -11,9 +19,13 @@ export function calculate(values) {
 }
 
 export function toOpposite(values) {
-  const elements = values.split(/[+\-*%//]/);
-  if (values.length !== elements.length) {
-    return `-(${ values })`;
+  try {
+    const elements = values.split(/[+\-*%//]/);
+    if (values.length !== elements.length) {
+      return `-(${ values })`;
+    }
+    return values * -1;
+  } catch (error) {
+    return 0;
   }
-  return values * -1;
 }
